d022038f7ef6e231d79a387732377875cff5457e
github yujialves <yujialves@i.softbank.jp>
2020-01-23 11:01:16 +0900

    ニックネームをクリックでプロフィール画面が見れるようにした

--- app/controller/ProfileHandlers.py
+++ app/controller/ProfileHandlers.py
@@ -53,3 +53,27 @@ class ProfileShowHandler(SigninBaseHandler):
             profile=pf,
             mode="",
             errors=[])
+
+class ProfileShowReadonlyHandler(SigninBaseHandler):
+    def get(self, user_id, thread_id):
+        if not self.current_user:
+            self.redirect("/signin")
+            return
+        # サインインユーザーの取得
+        _id = tornado.escape.xhtml_escape(self.current_user)
+        _signedInUser = user.find(user_id)
+
+        # モデルから全てのユーザ情報を取得
+        pf = profile.select_by_user_id(user_id)
+        if pf.attr["nick_name"] is None:
+            pf.attr["nick_name"] = ""
+        if pf.attr["introduction"] is None:
+            pf.attr["introduction"] = ""
+
+        # profile.htmlへリダイレクト
+        self.render("profile.html",
+            user=_signedInUser,
+            profile=pf,
+            mode="readonly",
+            thread=thread_id,
+            errors=[])
